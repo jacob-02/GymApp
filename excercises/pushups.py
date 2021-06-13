60d8556fbc212e7ddb99a29a54b4949fc19f12b2
jackjack3006 <jvsanoj23@gmail.com>
2021-06-13 11:43:59 +0530

    Creating a pushup counter

--- excercises/pushups.py
+++ excercises/pushups.py
@@ -3,13 +3,14 @@ from landmark import PoseModule
 import time
 
 
-def pushups(n = 0):
+def pushups(n=0):
     wCam, hCam = 500, 370
 
     capture = cv2.VideoCapture(n)
     capture.set(3, wCam)
     capture.set(4, hCam)
     pTime = 0
+    count = 0
 
     detector = PoseModule.PoseDetector(detectionCon=0.8)
 
@@ -31,9 +32,13 @@ def pushups(n = 0):
             cv2.circle(frame, (x3, y3), 7, (0, 255, 0), cv2.FILLED)
             cv2.circle(frame, (x4, y4), 7, (0, 255, 0), cv2.FILLED)
 
-            if (x1 - x3) != 0 | (x2 - x1) != 0 | (x4 - x2) != 0:
+            if (x1 - x3) != 0.0:
                 m1 = (y1 - y3) / (x1 - x3)
+
+            if (x2 - x1) != 0.0:
                 m = (y2 - y1) / (x2 - x1)
+
+            if (x4 - x2) != 0.0:
                 m2 = (y4 - y2) / (x4 - x2)
 
             if (1 + m * m1) != 0.0 or (1 + m * m2) != 0.0:
@@ -41,7 +46,15 @@ def pushups(n = 0):
                 beta = (m2 - m) / (1 + m * m2)
 
                 print(alpha, beta)
+            
+            if(beta <= 0.06):
+                n = 0
 
+            if(beta >= 2.3 and n == 0):
+                n = 1
+                count += 1
+                cv2.putText(frame, str(count), (250, 150), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                            fontScale=5, color=(0, 255, 0), thickness=2)
 
         cTime = time.time()
         fps = 1 / (cTime - pTime)
